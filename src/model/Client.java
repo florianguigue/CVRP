cb0f170819b17c5ce9552096b890ce92f0421611
Florian Guigue <florian.guigue@etu.univ-lyon1.fr>
2018-03-30 14:17:32 +0200

    init

---
+++ src/model/Client.java
@@ -0,0 +1,23 @@
+package model;
+
+public class Client {
+    private int tournee;
+
+    private int position;
+
+    public int getTournee() {
+        return tournee;
+    }
+
+    public void setTournee(int tournee) {
+        this.tournee = tournee;
+    }
+
+    public int getPosition() {
+        return position;
+    }
+
+    public void setPosition(int position) {
+        this.position = position;
+    }
+}
