4ca3230447c3bd4edeed22456ca220d928b6fd1e
Florian Guigue <florian.guigue@etu.univ-lyon1.fr>
2018-04-27 08:20:05 +0200

    init

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+import graph.Graph;
+
+public class Main {
+    public static void main(String[] args) {
+        Graph graph = new Graph();
+        graph.getClients();
+        System.out.println();
+    }
+}
