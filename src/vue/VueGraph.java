d32457fe932778501a02faace01e32d05990d18e
Oyanel <th.brune@laposte.net>
2018-06-14 10:20:31 +0200

    avec view sans couleur  - no listener

--- src/vue/VueGraph.java
+++ src/vue/VueGraph.java
@@ -15,13 +15,35 @@ import model.Client;
 import model.Liaison;
 
 public class VueGraph extends Application {
+
+    private static Graph graph;
+
     @Override
     public void start(Stage primaryStage) throws Exception {
 
+        graph = new Graph();
+        Group group;
+
+        System.out.println(graph.getFitness());
+        graph = graph.runOpti();
+        System.out.println(graph.getFitness());
+        group = draw();
+
+        Scene scene = new Scene(group, 1000, 1000);
+
+        primaryStage.setTitle("VRP graph");
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+
+    public Group draw(){
         Graph graph = new Graph();
         Group root =  new Group();
         for (Client client : graph.getClients()) {
             Circle circle = new Circle((client.getLongitude() * 5) + 50, (client.getLatitude() * 5 + 50), 2);
+            if (client.getId() == 0) {
+                
+            }
             root.getChildren().add(circle);
         }
 
@@ -29,12 +51,7 @@ public class VueGraph extends Application {
             Line line = new Line((liaison.getSource().getLongitude() * 5) + 50, (liaison.getSource().getLatitude() * 5) + 50, (liaison.getDestination().getLongitude() * 5) + 50, (liaison.getDestination().getLatitude() * 5) + 50);
             root.getChildren().add(line);
         }
-
-        Scene scene = new Scene(root, 1000, 1000);
-
-        primaryStage.setTitle("VRP graph");
-        primaryStage.setScene(scene);
-        primaryStage.show();
+        return  root;
     }
 
     public static void main(String args[]) {
